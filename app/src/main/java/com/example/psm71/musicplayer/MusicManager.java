5b5a8daf0f7f2c7772fdd95fc19297f41a904bd3
BuildTools <psm7177@naver.com>
2017-08-03 18:19:10 +0900

    파일 서치 완료

--- app/src/main/java/com/example/psm71/musicplayer/MusicManager.java
+++ app/src/main/java/com/example/psm71/musicplayer/MusicManager.java
@@ -1,7 +1,11 @@
 package com.example.psm71.musicplayer;
 
+import android.os.Environment;
+import android.util.Log;
+
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by psm71 on 2017-08-03.
@@ -9,26 +13,40 @@ import java.util.ArrayList;
 
 class MusicManager
 {
-    ArrayList <Music_info> MusicList = new ArrayList<Music_info>();
 
-    static  void FileSearch(ArrayList<Music_info> MusicList, String path, String directory)
+    static ArrayList <Music_info> MusicList = new ArrayList<Music_info>();
+    static ArrayList<String> extension = new ArrayList<String>();
+    MusicManager()
     {
-        String current_path = path+"/"+directory;
+        extension.add("mp3");
+        extension.add("flac");
+        FileSearch(MusicList, Environment.getExternalStorageDirectory().getAbsolutePath(),"");
+        FileSearch(MusicList, "/storage/external_SD","");
+    }
+    void FileSearch(ArrayList<Music_info> MusicList, String path, String directory)
+    {
+        String current_path = path+directory;
         File current_file = new File(current_path);
-        File[] list = current_file.listFiles();
-        for (int i = 0; i < list.length; i++)
+        File[] filelist = current_file.listFiles();
+        Log.d("path: ",current_path);
+        for (int i = 0; i < filelist.length; i++)
         {
-            File file = list[i];
-            if (file.isDirectory())
+            File file = filelist[i];
+            String extension= file.getName().substring(file.getName().lastIndexOf(".")+1,file.getName().length());
+            if (file.isDirectory() && !file.isHidden())
             {
                 String goto_directory = file.getName();
-                FileSearch(MusicList, current_path, goto_directory);
+                FileSearch(MusicList, current_path, "/"+goto_directory);
+                Log.d("Directory: ",goto_directory);
             }
-            else
+            else if(this.extension.contains(extension))
             {
-                MusicList.add(new Music_info(file.getPath(),""));// Music_info 생성자 추가 바람
+                String fileName = file.getName();
+                Log.d("File: ",fileName);
+                //MusicList.add(new Music_info(file.getPath(),""));// Music_info 생성자 추가 바람
             }
         }
+
         return;
     }
 }
