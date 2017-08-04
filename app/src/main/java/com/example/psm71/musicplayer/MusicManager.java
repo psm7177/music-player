6130899f0c83d871b960ad724a564a97fee1ade8
ss0715jj <ss0713jj@naver.com>
2017-08-04 22:52:40 +0900

    브랜치 정리

--- app/src/main/java/com/example/psm71/musicplayer/MusicManager.java
+++ app/src/main/java/com/example/psm71/musicplayer/MusicManager.java
@@ -1,62 +1,84 @@
 package com.example.psm71.musicplayer;
 
-import android.os.Environment;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
+
+import com.example.psm71.musicplayer.model.Music_info;
 
 /**
  * Created by psm71 on 2017-08-03.
  */
 
-class MusicManager
-{
+public class MusicManager {
 
-    static ArrayList <Music_info> MusicList = new ArrayList<Music_info>();
-    static ArrayList<String> extension = new ArrayList<String>();
-    MusicList adpater;
-    MusicManager()
-    {
+    private static final String TAG = MusicManager.class.getSimpleName();
+
+    static ArrayList<Music_info> MusicList = new ArrayList<>();
+    private Context context;
+
+    public MusicManager(Context context) {
+        this.context = context;
+
+        //FileSearch(Adapter, "/storage/external_SD","");
+
     }
-    MusicManager(MusicList adapter)
-    {
-        this.adpater = adapter;
-        extension.add("mp3");
-        extension.add("flac");
-        FileSearch(MusicList, Environment.getExternalStorageDirectory().getAbsolutePath(),"");
-        FileSearch(MusicList, "/storage/external_SD","");
-        
-    }
-    void FileSearch(ArrayList<Music_info> MusicList, String path, String directory)
-    {
-        String current_path = path+directory;
+
+    public void FileSearch() {
+
+        /*String current_path = path+directory;
         File current_file = new File(current_path);
-        File[] filelist = current_file.listFiles();
-        Log.d("path: ",current_path);
+        File[] filelist = current_file.listFiles(new Mp3Filter());
+        Log.e(TAG, "" + filelist.length);
         for (int i = 0; i < filelist.length; i++)
         {
             File file = filelist[i];
+            Log.d(TAG, file.getName());
             String extension= file.getName().substring(file.getName().lastIndexOf(".")+1,file.getName().length());
             if (file.isDirectory() && !file.isHidden())
             {
                 String goto_directory = file.getName();
-                FileSearch(MusicList, current_path, "/"+goto_directory);
+                FileSearch(Adapter, current_path, "/"+goto_directory);
                 Log.d("Directory: ",goto_directory);
             }
             else if(this.extension.contains(extension))
             {
                 String fileName = file.getName();
                 Log.d("File: ",fileName);
-                //MusicList.add(new Music_info(file.getPath(),""));// Music_info 생성자 추가 바람
+                //Adapter.add(new Music_info(file.getPath(),""));// Music_info 생성자 추가 바람
             }
-        }
+        }*/
 
-        return;
+        String[] projection = {MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST
+        };
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+
+        while (cursor.moveToNext()) {
+            int Music_ID = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
+            int Album_ID = cursor.getInt(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
+            String path = Uri.withAppendedPath(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, "" + Music_ID).toString();
+            String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+            String album_url = ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), Album_ID).toString();
+            String singer = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+
+            Log.e(TAG, "path : " + path + "\n title : " + title + "\n singer : " + singer
+                    + "\n Album_url : " + album_url);
+
+            MusicList.add(new Music_info(path, title, singer, album_url));
+        }
     }
-    ArrayList<Music_info> ResturnList()
-    {
+
+    public ArrayList<Music_info> getList() {
         return MusicList;
     }
 }
---
+++ app/src/main/java/com/example/psm71/musicplayer/model/Music_info.java
@@ -0,0 +1,51 @@
+package com.example.psm71.musicplayer.model;
+
+
+public class Music_info {
+    private String path;
+    private String name;
+    private String singer;
+    private String album;
+
+    // album cover image
+
+    public Music_info(String path, String name, String singer, String album)
+    {
+        this.path = path;
+        this.name = name;
+        this.singer = singer;
+        this.album = album;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+}
